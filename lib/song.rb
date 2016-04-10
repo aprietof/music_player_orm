edab67d829fbfb275419042aac7fdf8292338d57
aprietof <adrianprieto00@gmail.com>
2016-04-10 15:34:51 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,39 @@
 class Song
 
-  attr_accessor :name, :album
-  attr_reader :id
+  attr_accessor :name, :album, :id
 
-  def initialize (name, album, id = nil)
+  def initialize
     @name = name
     @album = album
+  end
+
+  def self.new_from_db(row)
+    new_song = self.new  # self.new is the same as running Song.new
+    new_song.id = row[0]
+    new_song.name =  row[1]
+    new_song.album = row[2]
+    new_song  # return the newly created instance
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM songs WHERE name = ? LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.create_table
@@ -16,6 +44,7 @@ class Song
         album TEXT
       );
     SQL
+
     DB[:conn].execute(sql)
   end
 
@@ -24,6 +53,7 @@ class Song
       INSERT INTO songs (name, album)
       VALUES (?,?)
     SQL
+
     DB[:conn].execute(sql, self.name, self.album)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
