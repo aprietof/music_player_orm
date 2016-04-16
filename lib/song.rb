1103ba72031311375cd797fd5102dc9a2f96bf5d
aprietof <adrianprieto00@gmail.com>
2016-04-16 08:48:44 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,16 @@
 class Song
 
-  attr_accessor :name, :album, :id
+  attr_accessor :name, :album
+  attr_reader :id
 
-  def initialize
+  def initialize(id=nil, name, album)
+    @id = id
     @name = name
     @album = album
   end
 
   def self.new_from_db(row)
-    new_song = self.new  # self.new is the same as running Song.new
-    new_song.id = row[0]
-    new_song.name =  row[1]
-    new_song.album = row[2]
+    new_song = self.new(row[0],row[1],row[2])  # self.new is the same as running Song.new
     new_song  # return the newly created instance
   end
 
@@ -27,13 +26,9 @@ class Song
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT * FROM songs WHERE name = ? LIMIT 1
-    SQL
-
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
+    sql = "SELECT * FROM songs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Song.new(result[0], result[1], result[2])
   end
 
   def self.create_table
@@ -49,13 +44,21 @@ class Song
   end
 
   def save
-    sql = <<-SQL
-      INSERT INTO songs (name, album)
-      VALUES (?,?)
-    SQL
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO songs (name, album)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.album)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end
+  end
 
-    DB[:conn].execute(sql, self.name, self.album)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+  def update
+    sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.id)
   end
 
   def self.create(name:, album:)
