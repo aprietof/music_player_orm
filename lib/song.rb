e4c925d3fa937cce9764cd9f1db3c95c79bffad6
aprietof <adrianprieto00@gmail.com>
2016-04-16 13:44:24 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -67,4 +67,15 @@ class Song
     song
   end
 
+  def self.find_or_create_by(name:, album:)
+    song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
+    if !song.empty?
+      song_data = song[0]
+      song = self.new(song_data[0], song_data[1], song_data[2])
+    else
+      song = self.create(name: name, album: album)
+    end
+    song
+  end
+
 end
